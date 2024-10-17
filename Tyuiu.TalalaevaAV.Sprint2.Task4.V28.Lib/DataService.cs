f9ceb65683e5ccece21b7c81f822605114429bc5
Tyuiu.TalalaevaAV@outlook.com <Tyuiu.TalalaevaAV@outlook.com>
2024-10-17 22:43:16 +0500

    Task4

--- Tyuiu.TalalaevaAV.Sprint2.Task4.V28.Lib/DataService.cs
+++ Tyuiu.TalalaevaAV.Sprint2.Task4.V28.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.TalalaevaAV.Sprint2.Task4.V28.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res = x < y*2-10 ? Math.Pow((1 + (y + 2) / (x * x)), x) : ((2 * x * x - Math.Cos(y) * Math.Cos(y) + 12) / (y * y - Math.Sin(x * x) + 9));
+            double res = x < y*2-10 ? Math.Pow((1 + (y + 2) / (x * x)), x) : (2 * Math.Pow(x, 2) - Math.Cos(Math.Pow(y, 2)) + 12) / (Math.Pow(y, 2) - Math.Sin(x*x) + 9);
             return Math.Round(res,3);
         }
     }
