eabf87bf965b6836abe44313d7c816fc7ee1b80c
Tyuiu.TalalaevaAV@outlook.com <Tyuiu.TalalaevaAV@outlook.com>
2024-10-18 22:12:15 +0500

    Task6

---
+++ Tyuiu.TalalaevaAV.Sprint2.Task6.V3/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.TalalaevaAV.Sprint2.Task6.V3.Lib;
+namespace Tyuiu.TalalaevaAV.Sprint2.Task6.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2| Выполнила: Талалаева А.В. | ИИПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if – полная и короткая форма записи                      *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Талалаева Александра Васильевна | ИИПБ-24-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу,которая использует оператор switch вычисляет требуемое*");
+            Console.WriteLine("* значение и возвращает результат.                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            int value;
+            string res;
+            Console.WriteLine("Введите день:");
+            value=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if ((value<1) || (value>7)) 
+            {
+                res = "Введено неверное значение";
+            }
+            else
+            {
+                res = ds.FindDayName(value);
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
