36520b5403abb44c7e2cf5346f2a44ca7bbffdc9
Tyuiu.TalalaevaAV@outlook.com <Tyuiu.TalalaevaAV@outlook.com>
2024-10-18 22:13:47 +0500

    Task6

--- Tyuiu.TalalaevaAV.Sprint2.Task6.V3.Test/DataServiceTest.cs
+++ Tyuiu.TalalaevaAV.Sprint2.Task6.V3.Test/DataServiceTest.cs
@@ -8,13 +8,13 @@ namespace Tyuiu.TalalaevaAV.Sprint2.Task6.V3.Test
         public void TestMethod1()
         {
             DataService ds= new DataService();
-            Assert.AreEqual("Понедельник", ds.FindDayName(1));
-            Assert.AreEqual("Вторник", ds.FindDayName(2));
-            Assert.AreEqual("Среда", ds.FindDayName(3));
-            Assert.AreEqual("Четверг", ds.FindDayName(4));
-            Assert.AreEqual("Пятница", ds.FindDayName(5));
-            Assert.AreEqual("Суббота", ds.FindDayName(6));
-            Assert.AreEqual("Понедельник", ds.FindDayName(1));
+            Assert.AreEqual("понедельник", ds.FindDayName(1));
+            Assert.AreEqual("вторник", ds.FindDayName(2));
+            Assert.AreEqual("среда", ds.FindDayName(3));
+            Assert.AreEqual("четверг", ds.FindDayName(4));
+            Assert.AreEqual("пятница", ds.FindDayName(5));
+            Assert.AreEqual("суббота", ds.FindDayName(6));
+            Assert.AreEqual("воскресенье", ds.FindDayName(1));
 
             Assert.ThrowsException<ArgumentException>(() =>
             {
